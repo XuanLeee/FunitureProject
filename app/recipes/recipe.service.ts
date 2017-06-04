efdcb9c6f21d72ad1c14031f48df0a18bb23eef4
BrotherP <xuanli@Nun.local>
2017-06-04 11:17:51 -0400

    version 1.0

---
+++ app/recipes/recipe.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Desks and chairs',
+      'American Black Walnut and Walnut Veneer Care ',
+      'https://upload.wikimedia.org/wikipedia/en/1/12/American-Furniture.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('Egg Chair',
+      '30\"H x 71\"W x 17.5\"D',
+      'http://elephantroomcreative.com/wp-content/uploads/2016/08/Furniture-74.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+}
